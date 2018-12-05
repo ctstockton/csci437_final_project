5c7c640da6fb954e0161cb9354101406cd90b0b3
Corey Stockton <coreystockton4c@gmail.com>
2018-12-05 09:42:33 -0500

    Created instants of objects

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 #                                11/27/18                                      #
 ################################################################################
 import pygame
+from planet import Planet
+from player import Player
 
 def main():
     pygame.init()
@@ -14,6 +16,17 @@ def main():
     background = background.convert()
     background.fill((255, 255, 255))
     screen.blit(background, (0, 0))
+
+    #create planet and players
+    planet = Planet()
+
+    player1 = Player()
+    player2 = Player()
+
+    #group sprites
+    planetSprite = pygame.Sprite.group(planet)
+    playerSprites = pygame.Sprite.group(player1,player2)
+
     clock = pygame.time.Clock()
     keepGoing = True
     while keepGoing:
@@ -21,7 +34,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keepGoing = False
+                
+        #update sprites
+        planetSprite.update()
+        playerSprites.update()
 
+        #draw sprites
+        planetSprite.draw(screen)
+        playerSprites.draw(screen)
 
 if __name__ == "__main__":
     main()
